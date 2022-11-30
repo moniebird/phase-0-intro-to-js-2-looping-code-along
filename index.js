492128739c3b188a7bd403baec32a86dc2cd711d
moniebird <moniebird@mac.com>
2022-11-30 12:26:48 -0500

    commit tree 353e35442da23cd9e65fdee52dfd0b6d77503a81

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    const messages =[]
+    for (let i = 0; i < names.length; i++) {
+    
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+   
+   return messages;
+
+}
+
+console.log(writeCards(["john", "jerry", "jim"], "birthday"))
+
+
+
+
+function countDown() {
+   
+   let countDown = 10; 
+while (countDown > -1) {
+  console.log(countDown--);
+}
+   
+}
+
+
+
+   
